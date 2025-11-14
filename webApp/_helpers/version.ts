94b941a571c572d2dfdf0209e7d89d4715d00cf7
The Zilch <zitchaden@gmail.com>
2025-11-14 18:45:14 +0000

    chore: rebuild bundle after import path changes

--- webApp/_helpers/version.ts
+++ webApp/_helpers/version.ts
@@ -1,2 +1,2 @@
-export const CONFIG_VERSION = '31791e4';
-export const BUILD_TIME = '2025-11-14T18:42:17.935Z';
+export const CONFIG_VERSION = 'dbd8375';
+export const BUILD_TIME = '2025-11-14T18:45:07.490Z';
