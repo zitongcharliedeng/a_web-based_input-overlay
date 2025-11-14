5ea5788031101d97f43d63106075146b7db2cdfe
The Zilch <zitchaden@gmail.com>
2025-11-14 17:22:23 +0000

    build: update version.ts to 049d197

--- webApp/_helpers/version.ts
+++ webApp/_helpers/version.ts
@@ -1,2 +1,2 @@
-export const CONFIG_VERSION = 'e101f1e';
-export const BUILD_TIME = '2025-11-14T15:15:24.977Z';
+export const CONFIG_VERSION = '049d197';
+export const BUILD_TIME = '2025-11-14T17:22:16.771Z';
