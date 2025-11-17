93678b0d2ef97d6bca1b00ad91ef927f250013ea
The Zilch <zitchaden@gmail.com>
2025-11-17 11:26:55 +0000

    fix: add symlink for DesktopWrappedWebapp and fix preload TypeScript errors
    - Added symlink from DesktopWrappedWebapp/node_modules to _devTools/node_modules
    - Imported IpcRendererEvent type from electron
    - Added type annotations for all _event parameters in ipcRenderer callbacks
    - Fixes all remaining TypeScript strict mode errors

--- SourceCode/DesktopWrappedWebapp/preload.ts
+++ SourceCode/DesktopWrappedWebapp/preload.ts
@@ -1,4 +1,4 @@
-import { contextBridge, ipcRenderer } from 'electron';
+import { contextBridge, ipcRenderer, type IpcRendererEvent } from 'electron';
 
 interface GlobalKeyEvent {
 	keycode: number;
@@ -59,28 +59,28 @@ console.log('[Preload] Loading preload script...');
 
 contextBridge.exposeInMainWorld('electronAPI', {
 	onGlobalKeyDown: (callback: (data: GlobalKeyEvent) => void): void => {
-		ipcRenderer.on('global-keydown', (_event, data: GlobalKeyEvent) => callback(data));
+		ipcRenderer.on('global-keydown', (_event: IpcRendererEvent, data: GlobalKeyEvent) => callback(data));
 	},
 	onGlobalKeyUp: (callback: (data: GlobalKeyEvent) => void): void => {
-		ipcRenderer.on('global-keyup', (_event, data: GlobalKeyEvent) => callback(data));
+		ipcRenderer.on('global-keyup', (_event: IpcRendererEvent, data: GlobalKeyEvent) => callback(data));
 	},
 
 	onGlobalMouseMove: (callback: (data: GlobalMouseMoveEvent) => void): void => {
-		ipcRenderer.on('global-mousemove', (_event, data: GlobalMouseMoveEvent) => callback(data));
+		ipcRenderer.on('global-mousemove', (_event: IpcRendererEvent, data: GlobalMouseMoveEvent) => callback(data));
 	},
 	onGlobalMouseDown: (callback: (data: GlobalMouseButtonEvent) => void): void => {
-		ipcRenderer.on('global-mousedown', (_event, data: GlobalMouseButtonEvent) => callback(data));
+		ipcRenderer.on('global-mousedown', (_event: IpcRendererEvent, data: GlobalMouseButtonEvent) => callback(data));
 	},
 	onGlobalMouseUp: (callback: (data: GlobalMouseButtonEvent) => void): void => {
-		ipcRenderer.on('global-mouseup', (_event, data: GlobalMouseButtonEvent) => callback(data));
+		ipcRenderer.on('global-mouseup', (_event: IpcRendererEvent, data: GlobalMouseButtonEvent) => callback(data));
 	},
 
 	onGlobalWheel: (callback: (data: GlobalWheelEvent) => void): void => {
-		ipcRenderer.on('global-wheel', (_event, data: GlobalWheelEvent) => callback(data));
+		ipcRenderer.on('global-wheel', (_event: IpcRendererEvent, data: GlobalWheelEvent) => callback(data));
 	},
 
 	onGlobalGamepadState: (callback: (state: GlobalGamepadState) => void): void => {
-		ipcRenderer.on('global-gamepad-state', (_event, state: GlobalGamepadState) => callback(state));
+		ipcRenderer.on('global-gamepad-state', (_event: IpcRendererEvent, state: GlobalGamepadState) => callback(state));
 	},
 
 	isReadonly: (): boolean => {
